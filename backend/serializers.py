298a148d95a0af1a3f2ba49c73d7b811137e84a3
Pongvarid <pongvarid@gmail.com>
2021-07-06 09:08:34 +0700

    add data

--- backend/serializers.py
+++ backend/serializers.py
@@ -1,5 +1,37 @@
 from rest_framework.serializers import ModelSerializer
-from backend.models import Course, Vocabulary, Senses, Exercise
+from backend.models import Language, Course, Vocabulary, Senses, Exercise
 
 
+class LanguageSerializer(ModelSerializer):
 
+    class Meta:
+        model = Language
+        fields = '__all__'
+
+
+class CourseSerializer(ModelSerializer):
+
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+
+class VocabularySerializer(ModelSerializer):
+
+    class Meta:
+        model = Vocabulary
+        fields = '__all__'
+
+
+class SensesSerializer(ModelSerializer):
+
+    class Meta:
+        model = Senses
+        fields = '__all__'
+
+
+class ExerciseSerializer(ModelSerializer):
+
+    class Meta:
+        model = Exercise
+        fields = '__all__'
--- backend/urls.py
+++ backend/urls.py
@@ -4,5 +4,10 @@ from backend import views
 
 router = SimpleRouter()
 
+router.register(r'language', views.LanguageViewSet)
+router.register(r'course', views.CourseViewSet)
+router.register(r'vocabulary', views.VocabularyViewSet)
+router.register(r'senses', views.SensesViewSet)
+router.register(r'exercise', views.ExerciseViewSet)
 
 urlpatterns = router.urls
